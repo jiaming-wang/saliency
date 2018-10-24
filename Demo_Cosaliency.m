5ffa0b6fec23703d0a538044b9bd1edc8d154b35
Jiaming Wang <54988890@qq.com>
2018-10-24 09:51:26 +0800

    Initial commit

---
+++ Demo_Cosaliency.m
@@ -0,0 +1,53 @@
+%% The demo for Cluster-based Co-saliency Detection in multiple images
+
+clc;close all;clear;
+
+addpath('./COS_code');
+
+%% image set 
+para.img_set_name= 'football_player';
+para.img_path=['./img_data/co_saliency_data/', para.img_set_name, '/'];
+para.result_path = ['./img_output/', para.img_set_name, '/'];
+
+if (~exist(para.result_path, 'dir')) 
+    mkdir(para.result_path);
+end
+
+%% co-saliency parameters
+para.files_list=dir([para.img_path '*.jpg']);
+para.img_num=length(para.files_list);
+% image resize scale
+para.Scale=200; 
+%clustering number on multi-image
+para.Bin_num=min(max(2 * para.img_num,10),30);
+%clustering number on single-image
+para.Bin_num_single=6;
+
+%% read images
+data.image = cell(para.img_num,1);
+
+for img_idx = 1:para.img_num
+   data.image{img_idx} = imread([para.img_path, para.files_list(img_idx).name]);
+end
+
+%% cosaliency detection
+[ result.cos_map, result.All_img] = Cosaliency_main( data, para.img_num, para.Scale, para.Bin_num);
+
+%% single sliency detection
+result.single_map = Single_saliency_main( data, para.img_num, para.Scale, para.Bin_num_single);
+
+%% combine saliency map
+result.final_map = result.single_map .* result.cos_map;
+
+%% save the results
+figure(1),subplot(3,1,1), imshow(result.All_img),title('Input images');
+subplot(3,1,2), imshow(result.single_map),colormap(gray),title('Single Saliency');
+subplot(3,1,3), imshow(result.final_map),colormap(gray),title('Co-Saliency');
+Save_result( data, para, result);
+
+
+
+
+
+
+
---
+++ Demo_single.m
@@ -0,0 +1,29 @@
+% The demo for Cluster-based Saliency Detection in single image
+
+clc;close all;clear;
+
+addpath('./COS_code');
+
+para.img_path='./img_data/single_data/';
+para.result_path = './img_output/';
+para.img_name = '1.tif';
+
+%--- cluster number -------
+para.Bin_num_single=6;
+%---- scaling the image ---
+para.Scale=300;
+
+data.image{1}=imread([para.img_path para.img_name]);
+[orgH, orgW, channel]=size(data.image{1});
+
+% single sliency detection
+single_map = Single_saliency_main( data, 1, para.Scale, para.Bin_num_single);
+
+Saliency_Map_single=imresize(Gauss_normal(single_map), [orgH, orgW]);
+imwrite(data.image{1},[para.result_path  para.img_name(1:end-4) '_org.png'],'png');
+imwrite(Saliency_Map_single, [para.result_path  para.img_name(1:end-4) '_single.png'],'png');
+
+figure,subplot(1,2,1), imshow(data.image{1}),title('Input images');
+subplot(1,2,2),imshow(Saliency_Map_single),title('Single Saliency');
+
+
